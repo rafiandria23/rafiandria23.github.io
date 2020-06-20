4dd5d80ae1b93f6f34e0863c4d854f724c41564d
rafiandria23 <rafiandria23@gmail.com>
2020-06-20 13:19:22 +0700

    Switched default GitHub Pages branch to gh-pages

--- scripts/deploy-github.js
+++ scripts/deploy-github.js
@@ -5,7 +5,7 @@ const ghPages = require('gh-pages')
 ghPages.publish(
   'public',
   {
-    branch: 'master',
+    branch: 'gh-pages',
     repo: 'https://github.com/rafiandria23/rafiandria23.github.io.git',
   },
   () => console.log('Successfully deployed to GitHub Pages!')
