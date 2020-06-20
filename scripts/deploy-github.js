3258ad284f74b0630747bc7f23e633c2cebab43e
rafiandria23 <rafiandria23@gmail.com>
2020-06-20 13:15:55 +0700

    Cofigured gh-pages.

---
+++ scripts/deploy-github.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const ghPages = require('gh-pages')
+
+ghPages.publish(
+  'public',
+  {
+    branch: 'master',
+    repo: 'https://github.com/rafiandria23/rafiandria23.github.io.git',
+  },
+  () => console.log('Successfully deployed to GitHub Pages!')
+);
