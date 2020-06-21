10f3e6c0dbbb60763600a439039f33f53af8ecda
rafiandria23 <rafiandria23@gmail.com>
2020-06-21 15:12:23 +0700

    Removed custom Image component.

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,7 +3,6 @@
  */
 export { default as Header } from './header';
 export { default as Footer } from './footer';
-export { default as Image } from './image';
 export { default as Layout } from './layout';
 export { default as SEO } from './seo';
 
